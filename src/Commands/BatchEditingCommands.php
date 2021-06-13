550bda649bebf93d25e826e229f141549bdb267e
ToriChan <43945424+YUUKIToriyama@users.noreply.github.com>
2021-06-14 05:53:11 +0900

    不要な関数を削除

--- src/Commands/BatchEditingCommands.php
+++ src/Commands/BatchEditingCommands.php
@@ -19,6 +19,21 @@ use Exception;
  */
 class BatchEditingCommands extends DrushCommands {
   /**
+   * 任意のテーブル、任意のコラムに関して文字列置換を行なうことができます
+   * @param $table 
+   */
+  private function replacement($table, $condition_col, $condition_val, $field, $old_value, $new_value) {
+    // データベースに接続
+    $connection = \Drupal::database();
+    $connection->update($table)
+      ->condition($condition_col, $condition_val, "=")
+      ->expression($field, "REPLACE($field, :old_value, :new_value)", [
+        ":old_value" => $old_value,
+        ":new_value" => $new_value
+      ])
+      ->execute();
+  }
+  /**
    * 4つの編集ルールに関して一括処理を行ないます
    * @command batch_editing
    */
@@ -32,11 +47,12 @@ class BatchEditingCommands extends DrushCommands {
     // ・node.type = article, pageに対し実行
     // ・フィールドbodyに対し文字列置換を行なう
     // ・delicious->yummy, https://www.drupal.org->https://WWW.DRUPAL.ORG
+    $count = 0;
     try {
       foreach($nodes as $record) {
         if ($record->type != "recipe") {
           $this->replacement(
-            "node__body", 
+            "node_revision__body", 
             "entity_id", 
             $record->nid, 
             "body_value", 
@@ -44,30 +60,33 @@ class BatchEditingCommands extends DrushCommands {
             "yummy"
           );
           $this->replacement(
-            "node__body", 
+            "node_revision__body", 
             "entity_id", 
             $record->nid, 
             "body_value", 
             "https://www.drupal.org", 
             "https://WWW.DRUPAL.ORG"
           );
+          // 処理が終了したらカウントを1Upする
+          $count++;
         }
       }
     } catch (Exception $error) {
       echo($error->getMessage());
     } finally {
-      echo("編集ルール1おわり" . PHP_EOL);
+      echo("編集ルール1:" . $count . "件処理しました" . PHP_EOL);
     }
 
     // コンテンツ編集ルール2
     // ・node.type = pageに対してのみ実行
     // ・フィールドtitleに対し文字列置換を行なう
     // ・s/Umami/this site/g
+    $count = 0;
     try {
       foreach($nodes as $record) {
         if ($record->type == "page") {
           $this->replacement(
-            "node_field_data",
+            "node_field_revision",
             "nid", 
             $record->nid, 
             "title", 
@@ -79,18 +98,19 @@ class BatchEditingCommands extends DrushCommands {
     } catch (Exception $error) {
       echo($error->getMessage());
     } finally {
-      echo("編集ルール2おわり" . PHP_EOL);
+      echo("編集ルール2おわり" . $count . "件処理しました" . PHP_EOL);
     }
 
     // コンテンツ編集ルール3
     // ・node.type = recipeに対してのみ実行
     // ・フィールドRecipeInstructionに対し文字列置換
     // ・s/minutes/mins/g
+    $count = 0;
     try {
       foreach($nodes as $record) {
         if ($record->type == "recipe") {
           $this->replacement(
-            "node__field_recipe_instruction",
+            "node_revision__field_recipe_instruction",
             "entity_id",
             $record->nid,
             "field_recipe_instruction_value",
@@ -102,18 +122,19 @@ class BatchEditingCommands extends DrushCommands {
     } catch(Exception $error) {
       echo($error->getMessage());
     } finally {
-      echo("編集ルール3おわり" . PHP_EOL);
+      echo("編集ルール3おわり" . $count . "件処理しました" . PHP_EOL);
     }
 
     // コンテンツ編集ルール4
     // ・node.type = article,pageに対して実行
     // ・フィールド"title"に対し文字列置換
     // ・s/delicious/yummy/g
+    $count = 0;
     try {
       foreach($nodes as $record) {
         if ($record->type != "recipe") {
           $this->replacement(
-            "node__body", 
+            "node_revision__body", 
             "entity_id", 
             $record->nid, 
             "body_value", 
@@ -125,99 +146,7 @@ class BatchEditingCommands extends DrushCommands {
     } catch(Exception $error) {
       echo($error->getMessage());
     } finally {
-      echo("編集ルール4おわり" . PHP_EOL);
+      echo("編集ルール4おわり" . $count . "件処理しました" . PHP_EOL);
     }
-  }
-  /**
-   * @command batch_editing:showTitles
-   */
-  public function showTitles() {
-    $database = \Drupal::database();
-    $query = $database->query("SELECT title FROM node_field_data");
-    $result = $query->fetchAll();
-    var_dump($result);
-  }
-
-  /**
-   * @command batch_editing:fixTitle
-   */
-  public function fixTitle() {
-    // データベースに接続
-    $connection = \Drupal::service("database");
-    // テーブルnodeを読みに行き、nidとtypeを取得
-    $result = $connection->query("SELECT nid, type FROM node")->fetchAll();
-    foreach($result as $record) {
-      echo("nid" . $record->nid . " is " . $record->type . "\n");
-      if ($record->type == "recipe") {
-        echo("hoge\n");
-      } else {
-        echo("piyo\n");
-      }
-    }
-    //var_dump($result);
-    //$query = $connection->update("node_field_data")->field("title")
-  }
-
-  private function replacement($table, $condition_col, $condition_val, $field, $old_value, $new_value) {
-    // データベースに接続
-    $connection = \Drupal::database();
-    $connection->update($table)
-      ->condition($condition_col, $condition_val, "=")
-      ->expression($field, "REPLACE($field, :old_value, :new_value)", [
-        ":old_value" => $old_value,
-        ":new_value" => $new_value
-      ])
-      ->execute();
-  }
-
-
-  /**
-   * Command description here.
-   *
-   * @param $arg1
-   *   Argument description.
-   * @param array $options
-   *   An associative array of options whose values come from cli, aliases, config, etc.
-   * @option option-name
-   *   Description
-   * @usage batch_editing-commandName foo
-   *   Usage description
-   *
-   * @command batch_editing:commandName
-   * @aliases foo
-   */
-  public function commandName($arg1, $options = ['option-name' => 'default']) {
-    $this->logger()->success(dt('Achievement unlocked.'));
-  }
-
-  /**
-   * An example of the table output format.
-   *
-   * @param array $options An associative array of options whose values come from cli, aliases, config, etc.
-   *
-   * @field-labels
-   *   group: Group
-   *   token: Token
-   *   name: Name
-   * @default-fields group,token,name
-   *
-   * @command batch_editing:token
-   * @aliases token
-   *
-   * @filter-default-field name
-   * @return \Consolidation\OutputFormatters\StructuredData\RowsOfFields
-   */
-  public function token($options = ['format' => 'table']) {
-    $all = \Drupal::token()->getInfo();
-    foreach ($all['tokens'] as $group => $tokens) {
-      foreach ($tokens as $key => $token) {
-        $rows[] = [
-          'group' => $group,
-          'token' => $key,
-          'name' => $token['name'],
-        ];
-      }
-    }
-    return new RowsOfFields($rows);
   }
 }
